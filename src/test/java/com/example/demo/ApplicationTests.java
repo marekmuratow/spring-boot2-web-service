0a6ca301763bc60c02d03d3543a83c5df55dae4b
Marek Muratow <contact@entito.pl>
2018-11-14 14:53:26 +0100

    init

---
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
